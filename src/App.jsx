024cf9de28a8b382600f70b9740fbad48d53ddd2
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2024-05-03 06:08:28 +0300

    thinking in react component :50%

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import ButtonToggle from "./Components/ButtonToggle";
+import ListBox from "./Components/ListBox";
+import Logo from "./Components/Logo";
+import Main from "./Components/Main";
+import MoviesList from "./Components/MoviesList";
+import MoviesToWatch from "./Components/MoviesToWatch";
+import NavBar from "./Components/NavBar";
+import Result from "./Components/Result";
+import Search from "./Components/Search";
+import Summary from "./Components/Summary";
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [isOpen2, setIsOpen2] = useState(true);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [watched, setWatched] = useState(tempWatchedData);
+  return (
+    <>
+      <NavBar>
+        <Logo />
+        <Search />
+        <Result movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MoviesList movies={movies} />
+        </ListBox>
+        <ListBox>
+          <Summary watched={watched} />
+          <MoviesToWatch watched={watched} />
+        </ListBox>
+      </Main>
+    </>
+  );
+}
